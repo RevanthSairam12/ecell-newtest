99cb20c763dc207467d230859a2daf9e3ef4e200
RevanthSairam12 <revanth242a@gmail.com>
2025-07-06 21:28:14 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'export',
+    trailingSlash: true,
     images: {
+        unoptimized: true,
         domains: ["acertinity ui.com", "images.unsplash.com", "assets.aceternity.com","picsum.photos","avatar.vercel.sh","i.ibb.co"]
+    },
+    experimental: {
+        appDir: true,
     },
 };
 
